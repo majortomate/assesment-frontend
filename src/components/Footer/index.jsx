9ca8243d4dfed8a6d7bbd9f82e3c923ee149ea48
Carlos <mac@Majortomate.local>
2022-08-04 20:08:31 -0500

    hotfix: add minor fixes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './styles.scss';
 
-function Footer() {
-  return (
-    <footer>
-      <span>Copyright © 2022 Mr.Shop. All rights reserved. Built by Majortomate</span>
-    </footer>
-  );
-}
+const Footer = () => (
+  <footer>
+    <span>Copyright © 2022 Mr.Shop. All rights reserved. Built by Majortomate</span>
+  </footer>
+);
 
 export default Footer;
--- src/services/rating.js
+++ src/services/rating.js
@@ -1,16 +1,19 @@
 /* eslint-disable no-else-return */
 // eslint-disable-next-line consistent-return
 function rating(rate) {
-  if (rate <= 1) {
-    return '⭐';
-  } else if (rate <= 2 && rate > 1) {
-    return '⭐ ⭐';
-  } else if (rate <= 3 && rate > 2) {
-    return '⭐ ⭐ ⭐';
-  } else if (rate <= 4 && rate > 3) {
-    return '⭐ ⭐ ⭐ ⭐';
-  } else if (rate <= 5 && rate > 4) {
-    return '⭐ ⭐ ⭐ ⭐ ⭐';
+  switch (true) {
+    case (rate <= 1):
+      return '⭐';
+    case (rate <= 2 && rate > 1):
+      return '⭐ ⭐';
+    case (rate <= 3 && rate > 2):
+      return '⭐ ⭐ ⭐';
+    case (rate <= 4 && rate > 3):
+      return '⭐ ⭐ ⭐ ⭐';
+    case (rate <= 5 && rate > 4):
+      return '⭐ ⭐ ⭐ ⭐ ⭐';
+    default:
+      return 'No Stars';
   }
 }
 export default rating;
